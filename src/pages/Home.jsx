6913e1f838b7ff17890534f461b68abdc116f934
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-09-16 15:37:32 +0800

    feat: add aria-label on checkbox

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -161,6 +161,7 @@ function Home() {
               </label>
               <input
                 aria-labelledby="setComplete"
+                aria-label="isComplete"
                 className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 type="checkbox"
                 name="setComplete"
